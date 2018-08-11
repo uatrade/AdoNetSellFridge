85cc4509ec666f7ba99f78542f14cba829ef4260
uatrade <piskovtsev@gmail.com>
2018-08-11 12:06:43 +0300

    Add project files.

---
+++ SellingFridge/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace SellingFridge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txboxDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txBoxQuntity = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txBoxPrice = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnprint = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(29, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 128);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Покупатель";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(35, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Продавец";
+            // 
+            // txboxDate
+            // 
+            this.txboxDate.Location = new System.Drawing.Point(35, 262);
+            this.txboxDate.Name = "txboxDate";
+            this.txboxDate.ReadOnly = true;
+            this.txboxDate.Size = new System.Drawing.Size(144, 20);
+            this.txboxDate.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Дата";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.txBoxQuntity);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.txBoxPrice);
+            this.groupBox2.Controls.Add(this.comboBox3);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(305, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(196, 184);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Холодильник";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(138, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Кол-во";
+            // 
+            // txBoxQuntity
+            // 
+            this.txBoxQuntity.Location = new System.Drawing.Point(138, 80);
+            this.txBoxQuntity.Name = "txBoxQuntity";
+            this.txBoxQuntity.ReadOnly = true;
+            this.txBoxQuntity.Size = new System.Drawing.Size(52, 20);
+            this.txBoxQuntity.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Цена";
+            // 
+            // txBoxPrice
+            // 
+            this.txBoxPrice.Location = new System.Drawing.Point(10, 158);
+            this.txBoxPrice.Name = "txBoxPrice";
+            this.txBoxPrice.ReadOnly = true;
+            this.txBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.txBoxPrice.TabIndex = 4;
+            this.txBoxPrice.TextChanged += new System.EventHandler(this.txBoxPrice_TextChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(10, 80);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Модель";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(10, 37);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Производитель";
+            // 
+            // btnprint
+            // 
+            this.btnprint.Location = new System.Drawing.Point(315, 262);
+            this.btnprint.Name = "btnprint";
+            this.btnprint.Size = new System.Drawing.Size(121, 23);
+            this.btnprint.TabIndex = 9;
+            this.btnprint.Text = "Печатать чек";
+            this.btnprint.UseVisualStyleBackColor = true;
+            this.btnprint.Click += new System.EventHandler(this.btnprint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 294);
+            this.Controls.Add(this.btnprint);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txboxDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Selling Fridges";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txboxDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txBoxPrice;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnprint;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txBoxQuntity;
+    }
+}
+
---
+++ SellingFridge/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SellingFridge
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+        private SqlDataReader readSellers;
+        private SqlDataReader readFridges;
+        private SqlDataReader readModel;
+        private SqlDataReader readPrice;
+        private SqlDataReader readSellerId;
+        private SqlDataReader readCustomerId;
+        private SqlDataReader readFridgeId;
+        Timer timer;
+
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            //btnprint.Enabled = false;
+
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            conn.ConnectionString = cs;
+            try
+            {
+                SqlCommand commSellers = new SqlCommand();
+                SqlCommand commFridges = new SqlCommand();
+                conn = new SqlConnection(cs);
+
+                conn.Open();
+                commSellers = new SqlCommand("Select Sellers.lastname from [Fridges].[dbo].Sellers", conn);
+                commFridges = new SqlCommand("Select distinct FridgList.manufacture from [Fridges].[dbo].FridgList", conn);
+                readSellers = commSellers.ExecuteReader();
+                
+                while (readSellers.Read())
+                {
+                    comboBox1.Items.Add(readSellers[0]);
+                }
+                readSellers.Close();
+                readFridges = commFridges.ExecuteReader();
+                while (readFridges.Read())
+                {
+                    comboBox2.Items.Add(readFridges[0]);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            txboxDate.Text = System.DateTime.Now.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand commPrice = new SqlCommand();
+            conn = new SqlConnection(cs);
+
+            try
+            {
+                commPrice = new SqlCommand($"Select FridgList.price, FridgList.quantity from [Fridges].[dbo].FridgList where FridgList.model='{comboBox3.SelectedItem.ToString()}'", conn);
+                // txBoxPrice.Text = readPrice["price"].ToString();
+                conn.Open();
+                readPrice = commPrice.ExecuteReader();
+                while (readPrice.Read())
+                {
+                    txBoxPrice.Text = readPrice["price"].ToString();
+                    txBoxQuntity.Text = readPrice["quantity"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(comboBox2.SelectedItem.ToString());
+            comboBox3.Items.Clear();
+            comboBox3.Text = "";
+            try
+            {
+                SqlCommand commModel = new SqlCommand();
+                SqlCommand commPrice = new SqlCommand();
+                conn = new SqlConnection(cs);
+
+                conn.Open();
+                commModel = new SqlCommand($"Select FridgList.model from [Fridges].[dbo].FridgList where FridgList.manufacture='{comboBox2.SelectedItem.ToString()}'", conn);
+                readModel = commModel.ExecuteReader();
+
+                while (readModel.Read())
+                {
+                    comboBox3.Items.Add(readModel["model"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void txBoxPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnprint_Click(object sender, EventArgs e)
+        {
+            string custId = "";
+            string SellId = "";
+            string FridgId = "";
+            using (conn=new SqlConnection(cs))
+            {
+                SqlCommand commCustom = new SqlCommand();
+                SqlCommand commSell = new SqlCommand();
+                SqlCommand commFridg = new SqlCommand();
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+                SqlCommand command = conn.CreateCommand();
+                command.Transaction = transaction;
+                
+                try
+                {
+                    
+                    command.CommandText = $"Insert into [Fridges].[dbo].Customers(firstname, lastname) values('{textBox1.Text}', '{textBox2.Text}')";
+                    command.ExecuteNonQuery();
+
+                    commCustom =new SqlCommand( $"Select Customers.id from [Fridges].[dbo].Customers where Customers.firstname='{textBox1.Text}' and Customers.lastname='{textBox2.Text}'", conn);
+
+                    commCustom.Transaction= transaction;
+                    readCustomerId = commCustom.ExecuteReader();
+
+                    while (readCustomerId.Read())  //выбор ID покупателя
+                    {
+                        custId = readCustomerId[0].ToString();
+                    }
+                    readCustomerId.Close();
+
+                    commSell = new SqlCommand($"Select Sellers.Id from [Fridges].[dbo].Sellers Where Sellers.lastname='{comboBox1.SelectedItem.ToString()}'", conn);
+
+                    commSell.Transaction = transaction;
+                    readSellerId = commSell.ExecuteReader();
+
+                    while (readSellerId.Read())  //выбор ID продавца
+                    {
+                        SellId = readSellerId[0].ToString();
+                    }
+                    readSellerId.Close();
+
+
+                    commFridg = new SqlCommand($"Select FridgList.Id from [Fridges].[dbo].FridgList Where FridgList.manufacture='{comboBox2.SelectedItem.ToString()}' and FridgList.model='{comboBox3.SelectedItem.ToString()}'", conn);
+
+                    commFridg.Transaction = transaction;
+                    readFridgeId = commFridg.ExecuteReader();
+
+                    while (readFridgeId.Read())  //выбор ID продавца
+                    {
+                        FridgId = readFridgeId[0].ToString();
+                    }
+                    readFridgeId.Close();
+
+                    command.CommandText = $"Insert into [Fridges].[dbo].Selling(sellers_id, customers_id, fridges_id) values({SellId}, {custId}, {FridgId})";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = $"UPDATE [Fridges].[dbo].FridgList SET quantity = quantity-1 WHERE id = {FridgId}"; //удаляем из базы
+                    txBoxQuntity.Text = (Int32.Parse(txBoxQuntity.Text) - 1).ToString();
+
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show(ex.Message);
+                }
+
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
